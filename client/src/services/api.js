639a79f8195acdc17a54117eb306ff57100a58a9
TavinV <otavioviniciusads@gmail.com>
2025-09-21 17:17:48 -0300

    update API base URL to a fixed production endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NODE_ENV === 'production'
-        ? 'https://seu-dominio.com/api'
-        : 'http://localhost:3000/api/v1',
+    baseURL: 'https://helpdesk-b4zs.onrender.com/api/v1/'
 });
 
 export default api;
